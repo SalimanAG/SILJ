ea0718e0444629bf5abc689b6e110047a764376a
Léonel DOSSOU <dossouleonel94@gmail.com>
2021-01-30 14:47:36 +0100

    model complementaire

---
+++ src/models/prixImmeuble.model.ts
@@ -0,0 +1,9 @@
+import { ValeurLocative } from './valeurLocative.model';
+
+export class SiteMarcher {
+
+    constructor(public idPrixIm:number, public dateDebPrixIm:Date, public dateFinPrixIm:Date, public prixIm:number,
+         public valeurLocative:ValeurLocative){
+  
+    }
+  }
---
+++ src/models/typeImmeuble.model.ts
@@ -0,0 +1,8 @@
+
+
+export class TypeImmeuble {
+
+    constructor(public codeTypIm:String, public libTypIm:String){
+  
+    }
+  }
