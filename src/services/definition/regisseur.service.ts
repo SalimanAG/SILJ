8cd5217ff0a6a435eeedc98b7351b9d089292264
Léonel DOSSOU <dossouleonel94@gmail.com>
2021-01-30 16:05:14 +0100

    cor

--- src/services/definition/regisseur.service.ts
+++ src/services/definition/regisseur.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Regisseur } from '../../models/regisseur.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisseurService {
 
-  constructor() { }
+  private host:String = 'http://localhost:8080/perfora-gpc/v1';
+
+  constructor(private httpClient:HttpClient) { }
+
+  getAllRegisseur(){
+    return this.httpClient.get<Regisseur[]>(this.host+'/stock/regisseur/list');
+
+  }
+
+  getRegisseurById(code:String){
+    return this.httpClient.get<Regisseur[]>(this.host+'/stock/regisseur/byCodReg/'+code);
+
+  }
+
+  addRegisseur(corps:Regisseur){
+    return this.httpClient.post<Regisseur>(this.host+'/stock/regisseur/list', corps);
+  }
+
+  editRegisseur(code:String, corps:Regisseur){
+    return this.httpClient.put<Regisseur>(this.host+'/stock/regisseur/byCodReg/'+code, corps);
+  }
+
+  deleteRegisseur(code:String){
+    return this.httpClient.delete<Boolean>(this.host+'/stock/regisseur/byCodReg/'+code);
+  }
+
 }
