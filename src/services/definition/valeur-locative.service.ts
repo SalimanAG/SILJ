a6fd8744ecf57d551f6cc86570a24d7cf367f4f8
Léonel DOSSOU <dossouleonel94@gmail.com>
2021-01-30 17:37:49 +0100

    co

--- src/services/definition/valeur-locative.service.ts
+++ src/services/definition/valeur-locative.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ValeurLocative } from '../../models/valeurLocative.model';
+import { Immeuble } from '../../models/immeuble.model';
 import { TypeImmeuble } from '../../models/typeImmeuble.model';
 import { PrixImmeuble } from '../../models/prixImmeuble.model';
 @Injectable({
@@ -12,25 +12,25 @@ export class ValeurLocativeService {
 
   constructor(private httpClient:HttpClient) { }
 
-  getAllValeurLocative(){
-    return this.httpClient.get<ValeurLocative[]>(this.host+'/location/immeuble/list');
+  getAllImmeuble(){
+    return this.httpClient.get<Immeuble[]>(this.host+'/location/immeuble/list');
 
   }
 
-  getValeurLocativeById(code:String){
-    return this.httpClient.get<ValeurLocative[]>(this.host+'/location/immeuble/byCodImm/'+code);
+  getImmeubleById(code:String){
+    return this.httpClient.get<Immeuble[]>(this.host+'/location/immeuble/byCodImm/'+code);
 
   }
 
-  addValeurLocative(corps:ValeurLocative){
-    return this.httpClient.post<ValeurLocative>(this.host+'/location/immeuble/list', corps);
+  addImmeuble(corps:Immeuble){
+    return this.httpClient.post<Immeuble>(this.host+'/location/immeuble/list', corps);
   }
 
-  editValeurLocative(code:String, corps:ValeurLocative){
-    return this.httpClient.put<ValeurLocative>(this.host+'/location/immeuble/byCodImm/'+code, corps);
+  editImmeuble(code:String, corps:Immeuble){
+    return this.httpClient.put<Immeuble>(this.host+'/location/immeuble/byCodImm/'+code, corps);
   }
 
-  deleteValeurLocative(code:String){
+  deleteImmeuble(code:String){
     return this.httpClient.delete<Boolean>(this.host+'/location/immeuble/byCodImm/'+code);
   }
 
