f32e1f6e69a7791f2ecf964540c5e4f13c425518
Salim AG <salimanagonhealberic@gmail.com>
2021-04-29 13:57:19 +0100

    m

--- src/app/saisie/livraison/livraison.component.ts
+++ src/app/saisie/livraison/livraison.component.ts
@@ -8,6 +8,7 @@ import { exit } from 'process';
 import { Subject } from 'rxjs';
 import { data } from 'jquery';
 import { element } from 'protractor';
+import * as moment from 'moment';
 
 
 
@@ -50,14 +51,14 @@ export class LivraisonComponent implements OnInit {
   dtTrigger1: Subject<any> = new Subject<any>();
   ligneopcaisse:LigneOpCaisse[] = [];
   ligneForOp:LigneOpCaisse[] = [];
-  editligneOpCaisse:LigneOpCaisse = new LigneOpCaisse(0,0,'', new OpCaisse('', new Date(),'',true,'',new Date(),
-  new Caisse('','',new Arrondissement('', '','','', new Commune('','','','',new Departement('','',new Pays('','',''))))),
+  editligneOpCaisse:LigneOpCaisse = new LigneOpCaisse(0,0,'', new OpCaisse('', new Date(),'',true,'',new Date(), 
+  new Caisse('','',new Arrondissement('', '','','', new Commune('','','','',new Departement('','',new Pays('','',''))))), 
   new TypeRecette('',''), new ModePaiement('',''),new Exercice('', '', new Date(), new Date(), '', false),
   new Utilisateur('', '', '', '', '', false, new Service('', ''))),new Article('', '', false, false, false, false, 0, '', new Famille('', ''), new Uniter('', '')));
-
+ 
   @ViewChild(DataTableDirective, {static: false})
   dtElement: DataTableDirective;
-
+  
   opCaisse:OpCaisse[]=[];
   caisses : Affecter[]=[];
   correspondant : Correspondant = new Correspondant('', false, new Magasinier('', '', ''),
@@ -66,18 +67,19 @@ export class LivraisonComponent implements OnInit {
   correspondant1 : Correspondant [];
 
   //FormControl
-  debut = new FormControl((new Date()).toISOString().substring(0, 10));
-  fin = new FormControl((new Date()).toISOString().substring(0, 10));
+  debut = new FormControl(moment(new Date()).format("yyyy-MM-DDTHH:mm"));
+  fin = new FormControl(moment(new Date()).format("yyyy-MM-DDTHH:mm"));
   opCaisseLivre = new FormControl('');
 
    initialised:boolean = false;
    magasinMagasinierConnected: Magasin = null;
    articleLigneOpcaisseLivre:LigneOpCaisse = null;
-
+  
 
   constructor(private servOp:OperationCaisseService, private serviceUser:UtilisateurService, private serviceCorres:
     CorrespondantService, private fbuilder:FormBuilder) {
     this.initDtOptions();
+    moment.locale('fr');
    }
 
   initDtOptions(){
@@ -109,27 +111,31 @@ export class LivraisonComponent implements OnInit {
       (data) => {
         data.forEach((element,index) => {
           if(element.livre===false && element.article.livrableArticle===true && element.opCaisse.typeRecette.codeTypRec==="P"
-          && element.opCaisse.caisse.codeCaisse===this.opCaisseLivre.value
+          && element.opCaisse.caisse.codeCaisse===this.opCaisseLivre.value 
           &&  new Date(element.opCaisse.dateOpCaisse).valueOf()>= new Date(this.debut.value).valueOf() && new Date(element.opCaisse.dateOpCaisse).valueOf()<= new Date(this.fin.value).valueOf())
           {
             this.ligneopcaisse.push(element);
-
+            
             console.log('verifi',this.ligneopcaisse);
-
-
+            
+            
           }
+          console.log("xxx", new Date(this.debut.value)); 
+          console.log("yyy",element.opCaisse.dateOpCaisse);
+          
+          
 
-
-        });
+          
+        }); 
         this.dtTrigger1.next();
-
+        
       },
       (erreur) => {
         console.log('Erreur lors de la récupération de la liste des actes livrables non livré ', erreur);
       }
     );
-
-
+    
+    
 
     this.servOp.getAllAffectations()
     .subscribe(
@@ -139,7 +145,7 @@ export class LivraisonComponent implements OnInit {
           if(element.utilisateur.idUtilisateur === this.serviceUser.connectedUser.idUtilisateur)
           this.caisses.push(element);
          // console.log("+-+-",this.caisses);
-
+          
         });
         //this.caisses=data;
       },
@@ -148,12 +154,12 @@ export class LivraisonComponent implements OnInit {
       }
     );
 
-    // Recupération du magasin de l'utilisateur connecté
+    // Recupération du magasin de l'utilisateur connecté 
 
     //var magasinMagasinierConnected: Magasin = null;
     this.serviceCorres.getAllCorres().subscribe(
       (data2) => {
-
+        
         data2.forEach(element2 => {
           if (element2.utilisateur != null && element2.utilisateur.idUtilisateur == this.serviceUser.connectedUser.idUtilisateur)
           {
@@ -167,7 +173,7 @@ export class LivraisonComponent implements OnInit {
                        this.magasinMagasinierConnected = element3.magasin;
                        console.log("++++", this.magasinMagasinierConnected);
                        exit;
-
+                      
                     }
 
                   });
@@ -176,7 +182,7 @@ export class LivraisonComponent implements OnInit {
                   console.log('Erreur lors de relation gerer', erreur);
                 }
               );
-
+              
               exit;
           }
 
@@ -195,42 +201,44 @@ export class LivraisonComponent implements OnInit {
     this.ligneopcaisse = [];
     $('#actualise').dataTable().api().destroy();
     this.dtTrigger1.next();
-
+   
 
 
     console.log('Chargement 1', this.ligneopcaisse.toString());
-
+    
     this.servOp.getAllOpLines().subscribe(
       (data) => {
         data.forEach((element,index) => {
-          console.log('1', element.opCaisse.dateOpCaisse, '2', this.fin.value,'3', new Date(this.fin.value));
           if(element.livre===false && element.article.livrableArticle===true && element.opCaisse.typeRecette.codeTypRec==="P"
-          &&  element.opCaisse.caisse.codeCaisse===this.opCaisseLivre.value
+          &&  element.opCaisse.caisse.codeCaisse===this.opCaisseLivre.value 
           && new Date(element.opCaisse.dateOpCaisse).valueOf()>= new Date(this.debut.value).valueOf() && new Date(element.opCaisse.dateOpCaisse).valueOf()<= new Date(this.fin.value).valueOf())
           {
             this.ligneopcaisse.push(element);
-
+            
             console.log('verifi',this.ligneopcaisse);
-
-
+            
+            
           }
+          
+        }); 
 
-        });
-
-
+        
+        
+        
+        
       },
       (erreur) => {
         console.log('Erreur lors de la récupération de la liste des actes livrables non livré ', erreur);
       }
     );
-
+    
   }
 
   verifierRecuperation()
   {
-
+    
     this.chargerInformations();
-
+   
   }
 
   validerLivraison(inde:number)
@@ -240,11 +248,16 @@ export class LivraisonComponent implements OnInit {
       this.editligneOpCaisse =  this.ligneopcaisse[inde];
       this.articleLigneOpcaisseLivre = this.editligneOpCaisse;
       this.editligneOpCaisse.livre=true;
+      console.log("magasin", this.magasinMagasinierConnected);
+      
+      this.editligneOpCaisse.magasin = this.magasinMagasinierConnected;
       this.servOp.editOpLine(this.editligneOpCaisse.idLigneOperCaisse,this.editligneOpCaisse).subscribe(
         (data) => {
-
+          console.log(data);
+          
+          
           this.chargerInformations();
-
+          
         },
         (erreur) => {
           console.log('Erreur lors de la récupération ', erreur);
@@ -255,19 +268,23 @@ export class LivraisonComponent implements OnInit {
       //essaie des vérification nécessaire
       console.log("***",this.magasinMagasinierConnected);
       console.log("+++",this.articleLigneOpcaisseLivre.article);
-
+    
      this.serviceCorres.getAllStocker().subscribe(
         (data) => {
           data.forEach(element => {
-
+            
             if ( element.magasin.codeMagasin == this.magasinMagasinierConnected.codeMagasin && element.article.codeArticle == this.articleLigneOpcaisseLivre.article.codeArticle)
             {
                 concernedStocker = element;
                 concernedStocker.quantiterStocker = concernedStocker.quantiterStocker+(-this.articleLigneOpcaisseLivre.qteLigneOperCaisse);
                 this.serviceCorres.editAStocker(concernedStocker.idStocker.toString(), concernedStocker).subscribe(
                   (data9) => {
-                    console.log("QA",data9);
+                    console.log("QA",data9); 
 
+                    //modification de la ligne operation de caisse par le magasin livreur
+                    //this.articleLigneOpcaisseLivre.magasin = this.magasinMagasinierConnected;
+                    
+                    
                   },
                   (erreur) => {
                     console.log('Erreur lors de la modification du Stocker pour réajustement du stock', erreur);
@@ -275,20 +292,22 @@ export class LivraisonComponent implements OnInit {
                 );
                 exist = true;
                 exit;
-
+              
             }
 
-
-
+            
+           
           });
 
           if(exist == false)
           {
           this.serviceCorres.addAStocker(new Stocker(-this.articleLigneOpcaisseLivre.qteLigneOperCaisse, 0, 0, 0, this.articleLigneOpcaisseLivre.article, this.magasinMagasinierConnected)).subscribe(
           (data) => {
-            console.log("990",data);
+            console.log("990",data); 
 
-
+            //Modiification de la ligne operation de caisse concerné par le magasin livreur
+            
+            
           },
           (erreur) => {
             console.log('Erreur lors de lAjout du stocker', erreur);
@@ -303,12 +322,12 @@ export class LivraisonComponent implements OnInit {
 
       );
 
-
+      
 
 
   }
 
 
-
+ 
 
 }
