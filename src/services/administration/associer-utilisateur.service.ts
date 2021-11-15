5c7b56f8fe6ab8a0dac479a2337e3257568fe6fc
COMPUTER <dossouleonel94@gmail.com>
2021-11-15 17:13:01 +0100

    deploiement du frontend

--- src/services/administration/associer-utilisateur.service.ts
+++ src/services/administration/associer-utilisateur.service.ts
@@ -9,7 +9,7 @@ import { AffectUserToArrondi } from '../../models/affectUserToArrondi.model';
 })
 export class AssocierUtilisateurService {
 
-  public adresseIp: string = '192.168.0.18:8081/Perfora';
+  public adresseIp: string = '192.168.0.2:8080/Perfora';
                              //'192.168.0.18:8081/Perfora'
 
   private host:String = 'http://'+this.adresseIp+'/perfora-gpc/v1';
